ecd06e3384b2bd026d5de8d185371edf5b8edd6d
Hatice Zehra Türel <90724716+zehraturel@users.noreply.github.com>
2024-08-04 23:59:33 +0300

    Last Commit -2 04.08.2024

--- src/main/java/ETicaret/Eticaret/EticaretApplication.java
+++ src/main/java/ETicaret/Eticaret/EticaretApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EticaretApplication {
 
     public static void main(String[] args) {
-
+//
         SpringApplication.run(EticaretApplication.class, args);
 
     }
